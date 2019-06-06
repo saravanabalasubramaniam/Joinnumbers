db9ea7b655fa117dd1251ac0058169fb994a963b
saravana <saravanasaru14313@gmail.com>
2019-06-06 14:29:23 +0530

    Update and rename README.md to Joinnumbers.java

---
+++ Joinnumbers.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+ class Joinnumbers
+{
+  public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	String a=input.nextLine();
+    String b=input.nextLine();
+    String c=a+b;
+    System.out.println(c);
+  }
+}
